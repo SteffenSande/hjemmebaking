6606712297c8998650da74454d790574248c7ccf
Steffen <Steffen.Sande@bekk.no>
2021-01-17 15:59:10 +0100

    feat: test

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import * as express from "express";
-import * as cors from "cors';
+import * as cors from "cors";
 
 admin.initializeApp();
 const db = admin.firestore();
--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,52 @@
 import React from "react";
-import Oppskrift from "./Oppskrift";
 import {ThemeProvider, CSSReset} from '@chakra-ui/core'
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import firebase from "firebase/app";
+import "firebase/analytics";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
+import Oppskrift from "./Oppskrift";
 import Oppskrifter from "./Oversikt/Oppskrifter";
 
-const App: React.FC = () => {
-  return (
-    <ThemeProvider>
-    <CSSReset />
-    <BrowserRouter>
-      <Route exact path="/" component={Oppskrifter} />
-      <Route exact path="/Oppskrift/:id" component={Oppskrift} />
-    </BrowserRouter>
-    </ThemeProvider>
-  );
-};
+function App() {
+    const [config, setConfig] = React.useState<Object>();
+
+    React.useEffect(() => {
+        fetch("/__/firebase/init.json")
+            .then((res) => res.json())
+            .then((c) => {
+                setConfig(c);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, []);
+
+    if (config && !firebase.apps.length) {
+        firebase.initializeApp(config);
+        firebase.analytics();
+    }
+
+    if (!firebase.apps.length) {
+        return null;
+    }
+
+    return (
+        <ThemeProvider>
+            <CSSReset />
+                <Router>
+                    <Switch>
+                        <Route path="/:oppskrift">
+                            <Oppskrift />
+                        </Route>
+                        <Route>
+                            <Oppskrifter />
+                        </Route>
+                    </Switch>
+                </Router>
+        </ThemeProvider>
+    );
+}
 
 export default App;
