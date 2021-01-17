3113bc8a360881f5f6261eb0c7af5521e52dd6dc
Steffen <Steffen.Sande@bekk.no>
2021-01-17 16:53:20 +0100

    tri

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,29 @@ import React from "react";
 import {ThemeProvider, CSSReset} from '@chakra-ui/core'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import firebase from "firebase/app";
+
+//ts-ignore
 import "firebase/analytics";
+//ts-ignore
 import "firebase/auth";
+//ts-ignore
 import "firebase/firestore";
+//ts-ignore
 import "firebase/storage";
+
 import Oppskrift from "./Oppskrift";
 import Oppskrifter from "./Oversikt/Oppskrifter";
 
+
 function App() {
     const [config, setConfig] = React.useState<Object>();
+    const appId = "1:694867289559:web:698949de787d2581ec5792";
 
     React.useEffect(() => {
         fetch("/__/firebase/init.json")
             .then((res) => res.json())
             .then((c) => {
-                setConfig(c);
+                setConfig({...c, appId: appId});
             })
             .catch((error) => {
                 console.error(error);
--- src/Oversikt/Oppskrifter.tsx
+++ src/Oversikt/Oppskrifter.tsx
@@ -1,6 +1,13 @@
 import React from "react";
+import useSWR from "swr";
+import {fetcher} from "../utils/fetcher";
 
 const Oppskrifter = () => {
+    const {data, error} = useSWR("/api/oppskrifter", fetcher);
+
+    if (error) return <h1>Noe gikk gale</h1>
+    if (!data) return <h1>Henter oppskrifter</h1>
+    console.log(data);
     return (
         <div>
             Her kommer det en liste med oppskriftene
